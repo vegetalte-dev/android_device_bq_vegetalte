a287ee7cb23e9a4cdf56ebe5c09aaecb021afbfe
Kra1o5 <kra1o5x@gmail.com>
2015-10-15 21:10:34 +0200

    keylayout: Rename to Headset & remove unused keys

--- device.mk
+++ device.mk
@@ -120,7 +120,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/ft5x06_ts.kl:system/usr/keylayout/ft5x06_ts.kl \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
-    $(LOCAL_PATH)/keylayout/msm8x16-skui-snd-card_Button_Jack.kl:system/usr/keylayout/msm8x16-skui-snd-card_Button_Jack.kl
+    $(LOCAL_PATH)/keylayout/Headset.kl:system/usr/keylayout/Headset.kl
 
 # Keystore
 PRODUCT_PACKAGES += \
