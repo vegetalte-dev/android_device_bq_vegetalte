8b1beb230beca21669e31a168b65444af064043c
Kra1o5 <kra1o5x@gmail.com>
2015-11-29 22:27:17 +0100

    vegetalte: Add data files for new ril blobs & update sec_config file

--- device.mk
+++ device.mk
@@ -76,6 +76,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.crda.sh
 
+# Data
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/dsi_config.xml:system/etc/data/dsi_config.xml \
+    $(LOCAL_PATH)/configs/netmgr_config.xml:system/etc/data/netmgr_config.xml \
+    $(LOCAL_PATH)/configs/qmi_config.xml:system/etc/data/qmi_config.xml
+
 # Display
 PRODUCT_PACKAGES += \
     copybit.msm8916 \
