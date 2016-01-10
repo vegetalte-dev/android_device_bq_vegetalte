70be9448e0109f5a9e645b297e5ce9dfaf93c57c
Christian Morlok <ChristianMorlok@gmail.com>
2016-01-10 15:32:32 +0100

    rootdir: Always start both ril-daemons
    There is no single SIM device. Always start both daemons.
    
    See https://github.com/piccolo-dev/android_device_bq_piccolo/issues/14
    
    Change-Id: I5f44519da91c11c1c4caf171d688f42341fb406b

--- device.mk
+++ device.mk
@@ -192,9 +192,6 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
     ueventd.qcom.rc
 
-PRODUCT_PACKAGES += \
-    init.qcom.ril.sh
-
 # USB
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.isUsbOtgEnabled=1
